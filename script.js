6eb3b48ad223e2f3005eb1af37fb9ac201bdbec8
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-10-19 17:05:04 +0200

    add a HTML form

--- script.js
+++ script.js
@@ -1,58 +1,62 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
 };
 
-
-function addBookToLibrary(title, author, pages, read) {
-
-
-
-
-    const book = new Book(title, author, pages, read);
-    
-    
+function addBookToLibrary(title, author, pages) {
+    const book = new Book(title, author, pages);
+        
     myLibrary.push(book);
 };
 
 
 
+const form = document.querySelector("form");
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+});
 
 
 
 
-addBookToLibrary("book1", "some", 222, true);
-addBookToLibrary("book2", "some", 333, false);
-addBookToLibrary("book3", "some", 444, true);
-addBookToLibrary("book4", "some", 555, false);
-console.log(myLibrary);
+addBookToLibrary("book1", "some", 222);
+addBookToLibrary("book2", "some", 333);
+addBookToLibrary("book3", "some", 444);
+addBookToLibrary("book4", "some", 555);
+// console.log(myLibrary);
 
 
-const books = document.querySelector(".books");
 
 for (let i = 0; i < myLibrary.length; i++) {
-    const book = document.createElement("div");
+    const books = document.querySelector(".books");
     
-    book.classList.add("book");
-    
-    const title = document.createElement("h1");
-    const author = document.createElement("p");
-    const pages = document.createElement("p");
-    const read = document.createElement("p");
-    
-    book.appendChild(title);
-    book.appendChild(author);
-    book.appendChild(pages);
-    book.appendChild(read);
+    books.innerHTML += `
+        <div class="book">
+            <h1>${myLibrary[i].title}</h1>
+            <p>Author: ${myLibrary[i].author}</p>
+            <p>Pages: ${myLibrary[i].pages}</p>
+        </div>
+    `;
 
-    title.textContent = myLibrary[i].title;
-    author.textContent = `Author: ${myLibrary[i].author}`;
-    pages.textContent = `Pages: ${myLibrary[i].pages}`;
-    read.textContent = `Read: ${myLibrary[i].read}`;
+    // const book = document.createElement("div");
+    
+    // book.classList.add("book");
+    
+    // const title = document.createElement("h1");
+    // const author = document.createElement("p");
+    // const pages = document.createElement("p");
+    
+    // book.appendChild(title);
+    // book.appendChild(author);
+    // book.appendChild(pages);
 
-    books.appendChild(book);
+    // title.textContent = myLibrary[i].title;
+    // author.textContent = `Author: ${myLibrary[i].author}`;
+    // pages.textContent = `Pages: ${myLibrary[i].pages}`;
+
+    // books.appendChild(book);
 };

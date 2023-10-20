28030e25165575e7f54a26d368219553493f1f4a
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-10-20 16:25:49 +0200

    add the remove button functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const library = document.querySelector(".library");
-
+const form = document.querySelector("form");
 
 const myLibrary = [];
 
@@ -8,7 +8,6 @@ function Book(title, author, pages) {
     this.author = author;
     this.pages = pages;
 };
-
 
 function addBookToLibrary(title, author, pages) {
     const newBook = new Book(title, author, pages);
@@ -34,56 +33,29 @@ function displayBooks() {
 
 
 
-library.addEventListener("click", event => {
-    console.log(
-        event.target
-    );
-});
 
 
 
 
-
-
-
-
-
-
-
-
-const form = document.querySelector("form");
 
 form.addEventListener("submit", event => {
     event.preventDefault();
-
+    
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value; 
     const pages = document.querySelector("#pages").value; 
-
+    
     addBookToLibrary(title, author, pages);
-
-    // displayNewBook();
+    
     
     event.target.reset();
 });
 
-
-
-
-// function displayNewBook() {
-//     const lastBookIndex = myLibrary.length - 1;
-//     const lastBook = myLibrary[lastBookIndex];
-
-
-//     books.innerHTML += `
-//     <div class="book" data-index="${lastBookIndex}">
-//         <h1>${lastBook.title}</h1>
-//         <p>Author: ${lastBook.author}</p>
-//         <p>Pages: ${lastBook.pages}</p>
-//         <button class="remove">Remove</button>
-//     </div>
-//     `;
-// }
+library.addEventListener("click", event => {
+    if (event.target.className === "remove") {
+        event.target.parentElement.remove();
+    };
+});
 
 
 
@@ -91,18 +63,9 @@ form.addEventListener("submit", event => {
 
 
 
-// addBookToLibrary("book1", "some", 222);
-// addBookToLibrary("book2", "some", 333);
-// addBookToLibrary("book3", "some", 444);
 
 
-// for (let i = 0; i < myLibrary.length; i++) {
-//     books.innerHTML += `
-//         <div class="book">
-//             <h1>${myLibrary[i].title}</h1>
-//             <p>Author: ${myLibrary[i].author}</p>
-//             <p>Pages: ${myLibrary[i].pages}</p>
-//             <button class="remove">Remove</button>
-//         </div>
-//     `;
-// };
+addBookToLibrary("book1", "some", 222);
+addBookToLibrary("book2", "some", 333);
+addBookToLibrary("book3", "some", 444);
+

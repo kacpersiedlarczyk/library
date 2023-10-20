5ac55a51cbf8be792425875888a8a47835c76bc4
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-10-20 16:07:11 +0200

    add the displayBooks function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const library = document.querySelector(".library");
+
+
 const myLibrary = [];
 
 function Book(title, author, pages) {
@@ -6,57 +9,100 @@ function Book(title, author, pages) {
     this.pages = pages;
 };
 
+
 function addBookToLibrary(title, author, pages) {
-    const book = new Book(title, author, pages);
-        
-    myLibrary.push(book);
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+
+    displayBooks();
 };
+
+function displayBooks() {
+    library.innerHTML = "";
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        library.innerHTML += `
+            <div class="book">
+                <h1>${myLibrary[i].title}</h1>
+                <p>Author: ${myLibrary[i].author}</p>
+                <p>Pages: ${myLibrary[i].pages}</p>
+                <button class="remove">Remove</button>
+            </div>
+        `;
+    };
+};
+
+
+
+library.addEventListener("click", event => {
+    console.log(
+        event.target
+    );
+});
+
+
+
+
+
+
+
+
+
 
 
 
 const form = document.querySelector("form");
 
 form.addEventListener("submit", event => {
-    event.preventDefault()
+    event.preventDefault();
+
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value; 
+    const pages = document.querySelector("#pages").value; 
+
+    addBookToLibrary(title, author, pages);
+
+    // displayNewBook();
+    
+    event.target.reset();
 });
 
 
 
 
-addBookToLibrary("book1", "some", 222);
-addBookToLibrary("book2", "some", 333);
-addBookToLibrary("book3", "some", 444);
-addBookToLibrary("book4", "some", 555);
-// console.log(myLibrary);
+// function displayNewBook() {
+//     const lastBookIndex = myLibrary.length - 1;
+//     const lastBook = myLibrary[lastBookIndex];
+
+
+//     books.innerHTML += `
+//     <div class="book" data-index="${lastBookIndex}">
+//         <h1>${lastBook.title}</h1>
+//         <p>Author: ${lastBook.author}</p>
+//         <p>Pages: ${lastBook.pages}</p>
+//         <button class="remove">Remove</button>
+//     </div>
+//     `;
+// }
 
 
 
-for (let i = 0; i < myLibrary.length; i++) {
-    const books = document.querySelector(".books");
-    
-    books.innerHTML += `
-        <div class="book">
-            <h1>${myLibrary[i].title}</h1>
-            <p>Author: ${myLibrary[i].author}</p>
-            <p>Pages: ${myLibrary[i].pages}</p>
-        </div>
-    `;
 
-    // const book = document.createElement("div");
-    
-    // book.classList.add("book");
-    
-    // const title = document.createElement("h1");
-    // const author = document.createElement("p");
-    // const pages = document.createElement("p");
-    
-    // book.appendChild(title);
-    // book.appendChild(author);
-    // book.appendChild(pages);
 
-    // title.textContent = myLibrary[i].title;
-    // author.textContent = `Author: ${myLibrary[i].author}`;
-    // pages.textContent = `Pages: ${myLibrary[i].pages}`;
 
-    // books.appendChild(book);
-};
+
+// addBookToLibrary("book1", "some", 222);
+// addBookToLibrary("book2", "some", 333);
+// addBookToLibrary("book3", "some", 444);
+
+
+// for (let i = 0; i < myLibrary.length; i++) {
+//     books.innerHTML += `
+//         <div class="book">
+//             <h1>${myLibrary[i].title}</h1>
+//             <p>Author: ${myLibrary[i].author}</p>
+//             <p>Pages: ${myLibrary[i].pages}</p>
+//             <button class="remove">Remove</button>
+//         </div>
+//     `;
+// };
